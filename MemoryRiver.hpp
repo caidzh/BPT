8ed60cd39f4c969102c3350ab214af2e5d920669
caidzh <148761690+caidzh@users.noreply.github.com>
2024-05-06 16:09:02 +0800

    Add files via upload

--- MemoryRiver.hpp
+++ MemoryRiver.hpp
@@ -1,6 +1,7 @@
 #ifndef BPT_MEMORYRIVER_HPP
 #define BPT_MEMORYRIVER_HPP
 
+// #include <iostream>
 #include <fstream>
 
 using std::string;
@@ -8,15 +9,37 @@ using std::fstream;
 using std::ifstream;
 using std::ofstream;
 
+
+
 template<class T, int info_len = 2>
 class MemoryRiver {
 public:
     fstream file;
     string file_name;
     int sizeofT = sizeof(T);
+    const int maxlen=40;
+    int len;
+    struct link{
+        T val;
+        int pos;
+        link *nxt,*prv;
+        link(){
+            nxt=prv=nullptr;
+        }
+    };
+    link *head,*tail;
     MemoryRiver() = default;
 
-    MemoryRiver(const string& file_name) : file_name(file_name) {}
+    MemoryRiver(const string& file_name) : file_name(file_name),len(0),head(nullptr),tail(nullptr) {}
+
+    ~MemoryRiver(){
+        link *ptr=head;
+        while(ptr){
+            link *nxt=ptr->nxt;
+            delete ptr;
+            ptr=nxt;
+        }
+    }
 
     void initialise(string FN = "") {
         if (FN != "") file_name = FN;
@@ -26,58 +49,112 @@ public:
             file.write(reinterpret_cast<char *>(&tmp), sizeof(int));
         file.close();
     }
-
-    //读出第 n 个 int 的值赋给 tmp，1_base
     void get_info(int &tmp, int n) {
         if (n > info_len) return;
-        /* your code here */
         file.open(file_name,std::ios::in);
         file.seekg((n-1)*sizeof(int));
         file.read(reinterpret_cast<char *>(&tmp),sizeof(int));
         file.close();
     }
-
-    //将 tmp 写入第 n 个 int 的位置，1_base
     void write_info(int tmp, int n) {
         if (n > info_len) return;
-        /* your code here */
         file.open(file_name,std::ios::in|std::ios::out);
         file.seekp((n-1)*sizeof(int));
         file.write(reinterpret_cast<char *>(&tmp),sizeof(int));
         file.close();
     }
-
-    //在文件合适位置写入类对象 t，并返回写入的位置索引 index
-    //位置索引意味着当输入正确的位置索引 index，在以下三个函数中都能顺利的找到目标对象进行操作
-    //位置索引 index 可以取为对象写入的起始位置
-    int write(T &t) {
-        /* your code here */
+    void shift_to_head(link *ptr){
+        if(!head)
+            head=tail=ptr;
+        if(ptr==head)
+            return;
+        if(ptr==tail)
+            tail=ptr->prv;
+        link *nxt=ptr->nxt,*prv=ptr->prv;
+        if(nxt)
+            nxt->prv=prv;
+        if(prv)
+            prv->nxt=nxt;
+        ptr->nxt=head;
+        head->prv=ptr;
+        ptr->prv=nullptr;
+        head=ptr;
+    }
+    void pop_back(){
+        link *prv=tail->prv;
+        prv->nxt=nullptr;
+        delete tail;
+        tail=prv;
+    }
+    // void check(){
+    //     link *ptr=head;
+    //     while(ptr){
+    //         std::cout<<ptr->pos<<" ";
+    //         ptr=ptr->nxt;
+    //     }
+    //     std::cout<<"\n";
+    // }
+    int write(T &t){
+        len++;
+        link *ptr=new link;
+        ptr->val=t;
         file.open(file_name,std::ios::in|std::ios::out);
         file.seekp(0,std::ios::end);
         int lst=file.tellp();
+        ptr->pos=lst;
+        shift_to_head(ptr);
+        if(len>maxlen)
+            pop_back(),len--;
         file.write(reinterpret_cast<char *>(&t),sizeofT);
         file.close();
         return lst;
     }
-
-    //用 t 的值更新位置索引 index 对应的对象，保证调用的 index 都是由 write 函数产生
-    void update(T &t, const int index) {
-        /* your code here */
+    void update(T &t, const int index){
+        link *ptr=head;
+        while(ptr!=nullptr&&ptr->pos!=index)
+            ptr=ptr->nxt;
+        if(ptr){
+            ptr->val=t;
+            shift_to_head(ptr);
+        }
+        else{
+            len++;
+            link *ptr=new link;
+            ptr->val=t;
+            ptr->pos=index;
+            shift_to_head(ptr);
+            if(len>maxlen)
+                pop_back(),len--;
+        }
         file.open(file_name,std::ios::in|std::ios::out);
         file.seekp(index);
         file.write(reinterpret_cast<char *>(&t),sizeofT);
         file.close();
     }
-
-    //读出位置索引 index 对应的 T 对象的值并赋值给 t，保证调用的 index 都是由 write 函数产生
     void read(T &t, const int index) {
-        /* your code here */
-        file.open(file_name,std::ios::in);
-        file.seekg(index);
-        file.read(reinterpret_cast<char *>(&t),sizeofT);
-        file.close();
+        link *ptr=head;
+        while(ptr!=nullptr&&ptr->pos!=index)
+            ptr=ptr->nxt;
+        if(ptr){
+            t=ptr->val;
+            shift_to_head(ptr);
+            return;
+        }
+        else{
+            len++;
+            link *ptr=new link;
+            ptr->pos=index;
+            file.open(file_name,std::ios::in);
+            file.seekg(index);
+            file.read(reinterpret_cast<char *>(&t),sizeofT);
+            file.close();
+            ptr->val=t;
+            shift_to_head(ptr);
+            if(len>maxlen)
+                pop_back(),len--;
+        }
     }
 };
 
 
-#endif //BPT_MEMORYRIVER_HPP
+#endif
